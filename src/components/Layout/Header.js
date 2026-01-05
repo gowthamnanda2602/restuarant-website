eca078a2b488b44b9627d60d2e3b6d14c0b59a04
gowthamnanda2602 <gowthamnanda2602@gmail.com>
2026-01-05 20:17:55 +0530

    enabled cart backdrop functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <React.Fragment>
     <header className={classes.header}>
       <h1>Restaurant Meals</h1>
-      <HeaderCartButton />
+      <HeaderCartButton onClick={props.onShowCart} />
     </header>
     <div className={classes['main-image']}>
         <img src={MealsImage} alt="A table full of delicious food!" />
